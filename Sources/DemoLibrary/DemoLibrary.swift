967838e26650740e714e9cf19fce4830ddd7d1da
avkuma <avinash.kumar@medlife.com>
2020-02-20 00:52:31 +0530

    first commint

--- Sources/DemoLibrary/DemoLibrary.swift
+++ Sources/DemoLibrary/DemoLibrary.swift
@@ -1,3 +1,14 @@
-struct DemoLibrary {
-    var text = "Hello, World!"
+import SwiftUI
+
+struct DemoLibrary: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+struct DemoLibrary_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        DemoLibrary()
+    }
 }
---
+++ Sources/DemoLibrary/NewView.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Avinash on 20/02/2020.
+//
+
+import Foundation
